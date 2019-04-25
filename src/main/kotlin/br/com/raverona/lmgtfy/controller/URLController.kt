81165dfb035f24eac8a4f33c885bc31c4dabe521
Rafael Marinaro Verona <rverona@uolinc.com>
2019-04-25 14:06:39 -0300

    Create an endpoint to explain users how this app works

--- src/main/kotlin/br/com/raverona/lmgtfy/controller/URLController.kt
+++ src/main/kotlin/br/com/raverona/lmgtfy/controller/URLController.kt
@@ -7,14 +7,14 @@ import br.com.raverona.lmgtfy.response.SlackResponse
 import br.com.raverona.lmgtfy.searchType.SearchType
 import br.com.raverona.lmgtfy.url.LmgtfyURL
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class URLController(@Value("\${lmgtfy.base.url}") val lmgtfyBaseURL: String) {
 
-    @RequestMapping(value = ["/"], method = [RequestMethod.POST], produces = ["application/json"], consumes = ["application/x-www-form-urlencoded"])
-    fun getLmgtfyURL(
-            slackPayload: SlackPayload): SlackResponse {
+    @RequestMapping(value = ["/"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun getLmgtfyURL(slackPayload: SlackPayload): SlackResponse {
 
         val lmgtfyURL = LmgtfyURL(
                 baseURL = lmgtfyBaseURL,
@@ -24,5 +24,19 @@ class URLController(@Value("\${lmgtfy.base.url}") val lmgtfyBaseURL: String) {
         )
 
         return SlackResponse(response_type = "in_channel", text = lmgtfyURL.toURLString())
+    }
+
+    @RequestMapping(value = ["/help"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun getHelpMessage(slackPayload: SlackPayload): SlackResponse {
+        return SlackResponse(
+                response_type = "in_channel",
+                text = """Hi ${slackPayload.user_name}, here's how I work:
+                    |type a command followed by a serch criteria and I'll answer you with a link
+                    |to solve your search needs based on the command used
+                    |the commands have the following format:
+                    |/(provider) (stuff I want to search)
+                    |available providers are: Aol, Ask, Bing, DuckDuckGo, Google and Yahoo
+                    |exemple: /google I don't know how to google stuff
+                """.trimMargin())
     }
 }
--- src/main/kotlin/br/com/raverona/lmgtfy/request/SlackPayload.kt
+++ src/main/kotlin/br/com/raverona/lmgtfy/request/SlackPayload.kt
@@ -3,7 +3,7 @@ package br.com.raverona.lmgtfy.request
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class SlackPayload(val text: String, val command: String) {
+class SlackPayload(val text: String, val command: String, val user_name: String) {
     val providerName: String
 
     init {
